89918f1684b8bd681c5d784027376185cfde5e10
it-site44-com <108983585+it-site44-com@users.noreply.github.com>
2023-12-29 17:53:06 +0000

    fix err maven + add file github_runer_thinhnq.sh

---
+++ cicd/github_actions/github_runer_thinhnq.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -x #echo on
+
+# ls
+# du -sh
+# whoami
+
+# STEP [INIT]
+# check current folder
+pwd
+
+
+
+# STEP [nginx_image]
+# build docker image
+docker build -t nginx_image ./html
+
+# remove container by name
+docker rm -f nginx_image
+
+# run container
+docker run --name nginx_image -p 80:80 -d nginx_image
+
+
+
+# STEP [jdk_image]
+# build docker image
+docker build -t jdk_image ./java
+
+# remove container by name
+docker rm -f jdk_image
+
+# run container
+docker run --name jdk_image -p 8081:8081 -d jdk_image
+
+
+
+# STEP [nodejs_image]
